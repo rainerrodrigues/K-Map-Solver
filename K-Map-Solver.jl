f0593fdd7a0fb7d02471670bdd88c8d8e8ffb32f
Rainer Rodrigues <rainerrodrigues@gmail.com>
2024-07-01 11:59:48 +0100

    Updateds the frame design for the userinterface structures

--- K-Map-Solver.jl
+++ K-Map-Solver.jl
@@ -1,2 +1,10 @@
-function k-map(list)
-    n = len(list) #Determine the length of the K-Map
+function k-map(list,num)
+    n = len(list) #Determine the length of the K-Map to chart the entire K-map
+    list_placeholder = list #Copy list for manipulation into appropriate position for K-Map calculation
+    if n == 4:
+        list[4] = list[3];
+        list[3] = list[4];
+    else:
+        print("To Do")
+    end
+    
