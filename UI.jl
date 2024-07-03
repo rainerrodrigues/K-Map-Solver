9735f435fda25e00a5deccd32bd609034d73dc3e
Rainer Rodrigues <rainerrodrigues@gmail.com>
2024-07-03 20:33:03 +0100

    Added modification to frame structure

--- UI.jl
+++ UI.jl
@@ -1,2 +1,33 @@
-function UserInterface():
+function UserInterface()
     print("Variables")
+end
+
+function Lines()
+    print(" ")
+    print(---)
+end
+
+function HoriLines()
+    println(" ")
+    println(|)
+    println(|)
+    println(|)
+end
+
+function Frames()
+    Lines()
+    Lines()
+    Lines()
+    Lines()
+    HoriLines()
+    HoriLines()
+    HoriLines()
+    Lines()
+    Lines()
+    Lines()
+    Lines()
+end
+
+function KStructures()
+    if 
+
