37f05a8f7deeb9a019d4f225aaabd2e704017446
Rainer Rodrigues <rainerrodrigues@gmail.com>
2024-07-17 20:58:43 +0100

    Modified UI using heatmap

--- UI.jl
+++ UI.jl
@@ -1,33 +1,17 @@
-function UserInterface()
-    print("Variables")
+using Plots
+
+#For simplicity, considering a 2x2 table
+table = [
+    0 1;
+    0 1
+]
+
+p = heatmap(table, c=:blues, xrotation = 0,yflip = true,aspect_ratio =:equal,color=:viridis,axis=false,title="K-Map")
+
+for i in 1:size(table,1)
+    for j in 1:size(table,2)
+        annotate!(j-0.5,i-0.5,text(string(table[i,j]),:white,:center))
+    end
 end
 
-function Lines()
-    print(" ")
-    print(---)
-end
-
-function HoriLines()
-    println(" ")
-    println(|)
-    println(|)
-    println(|)
-end
-
-function Frames()
-    Lines()
-    Lines()
-    Lines()
-    Lines()
-    HoriLines()
-    HoriLines()
-    HoriLines()
-    Lines()
-    Lines()
-    Lines()
-    Lines()
-end
-
-function KStructures()
-    if 
-
+display(p)
